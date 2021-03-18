baf7dc34e54628a53fa91e51a8ce9f8e46582d82
Taylor Collins <taylorcc33@gmail.com>
2021-03-18 15:17:06 -0600

    diceset methods defined

---
+++ DiceRoller/classes/Dice.rb
@@ -0,0 +1,6 @@
+class Dice
+
+  def roll
+    @dice_roll = 1 + rand(6)
+  end
+end
--- DiceRoller/classes/DiceSet.rb
+++ DiceRoller/classes/DiceSet.rb
@@ -2,3 +2,18 @@
 # DiceSet roll should roll both dice
 # rand(6) returns a random number from 0 to 5
 # DiceSet display should display the values of both dice
+
+require_relative "Dice.rb"
+
+class DiceSet 
+
+  @dice1 = Dice.new
+  @dice2 = Dice.new
+
+  def play
+    sum1 = @dice1.roll
+    sum2 = @dice2.roll
+
+    total_sum = sum1 + sum2
+  end
+end

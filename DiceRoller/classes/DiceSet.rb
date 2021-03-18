5bb38938b6fd95c78c4e96d714ced24279342257
Taylor Collins <taylorcc33@gmail.com>
2021-03-18 14:49:57 -0600

    dice files created

---
+++ DiceRoller/classes/DiceSet.rb
@@ -0,0 +1,4 @@
+# DiceSet instance contains 2 Dice instances
+# DiceSet roll should roll both dice
+# rand(6) returns a random number from 0 to 5
+# DiceSet display should display the values of both dice

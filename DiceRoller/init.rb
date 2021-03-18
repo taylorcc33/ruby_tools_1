995fe40bdabd5ae56b68833daddf1bab6d3dd575
Taylor Collins <taylorcc33@gmail.com>
2021-03-18 16:09:08 -0600

    dice set finished

--- DiceRoller/init.rb
+++ DiceRoller/init.rb
@@ -1,16 +1,25 @@
 #!/usr/bin/env ruby
 
+require_relative 'classes/Dice'
 require_relative 'classes/DiceSet'
 
-@dice_set = DiceSet.new
+puts "-" * 26
+puts "Welcome to the Dice Roller"
+puts "-" * 26
+puts
 
-def app
-  puts "Press 1 to roll dice"
-  input = gets.to_i
-  if input === 1
-    @dice_set.play
+dice_set = DiceSet.new
+puts dice_set.display
+
+response = nil
+until response == 'q'
+  puts "\nType 'r' to roll again, 'q' to quit"
+  print '> '
+  response = gets.chomp
+
+  if response == 'r'
+    dice_set.roll
+    puts "\n" + dice_set.display
   end
+  
 end
-
-app
-

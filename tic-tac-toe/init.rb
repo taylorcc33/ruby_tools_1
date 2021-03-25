178b5b1a3a30260b4250df58c29c575f8de81334
Taylor Collins <taylorcc33@gmail.com>
2021-03-24 21:35:27 -0600

    added main menu and exit method

--- tic-tac-toe/init.rb
+++ tic-tac-toe/init.rb
@@ -2,8 +2,26 @@ require_relative "board"
 
 @board = Board.new
 
+def main_menu
+  puts "~~~~~Welcom to Tic Tac Toe~~~~~"
+  puts "Choose an option:"
+  puts "1. New Game"
+  puts "2. Quit"
+
+  user_input = gets.chomp.to_i
+
+  if user_input === 1
+    new_game
+  elsif user_input === 2
+    exit
+  else
+    puts "invalid option"
+    main_menu
+  end
+end
+
 def new_game
   @board.generate_board
 end
 
-new_game
+main_menu

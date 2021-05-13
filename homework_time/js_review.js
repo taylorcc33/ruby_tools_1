a0ef1cd3a6b9c7856f7104a2260f5c7dacdab626
Taylor Collins <taylorcc33@gmail.com>
2021-05-13 13:13:34 -0600

    changes

--- homework_time/js_review.js
+++ homework_time/js_review.js
@@ -1,73 +1,45 @@
 let contacts = [
   {
-first_name : "Bruce",
-last_name : "wayne",
-phone :"888-516-7365",
-},
-{
-  first_name : "Steve",
-  last_name : "Rogers",
-  phone :"212-970-4133",
-},
-{
-  first_name : "Wade",
-  last_name : "Wilson",
-  phone :"914-555-1407",
-}
+    first_name: "Bruce",
+    last_name: "wayne",
+    phone: "888-516-7365",
+  },
+  {
+    first_name: "Steve",
+    last_name: "Rogers",
+    phone: "212-970-4133",
+  },
+  {
+    first_name: "Wade",
+    last_name: "Wilson",
+    phone: "914-555-1407",
+  },
 ];
 
 const mainMenu = () => {
-  
-  console.log(
-    "Please choose an option"
-  )
-  console.log(
-    "1 Add a contact"
-  )
-  console.log(
-    "2 view contacts"
-  )
-  console.log(
-    "3 Update a contact"
-  )
-  console.log(
-    "4 delete a contact"
-  )
+  console.log("Please choose an option");
+  console.log("1 Add a contact");
+  console.log("2 view contacts");
+  console.log("3 Update a contact");
+  console.log("4 delete a contact");
 
-  userInput = prompt(":")
+  userInput = prompt(":");
 
   if (userInput == 1) {
     addContact();
   } else if (userInput == 2) {
-    
   } else if (userInput == 3) {
-    
   }
-
-}
-
+};
 
 const addContact = () => {
-  console.log("Please type in contact name")
+  console.log("Please type in contact name");
 
-  nameInput = prompt(":")
+  nameInput = prompt(":");
+};
 
-  
+const test_function = () => {
+  let x = "Taylor";
 
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+  console.log(x);
+};
---
+++ homework_time/test1.rb
@@ -0,0 +1,54 @@
+user1 = {
+  id: 1,
+  name: "Tony",
+}
+user2 = {
+  id: 2,
+  name: "Sally",
+}
+res1 = {
+  id: 1,
+  name: "Spicy Thai",
+  location: {
+    city: "SLC",
+    state: "UT",
+  },
+  delivery: true,
+  days_open: "mon,tues,wed,thur,fri,sat,sun",
+  likes: 1000,
+  dishes: [
+    { name: "Pad Thai", price: 10.25, ingredients: ["noddles", "peppers"] },
+    { name: "Drunken Noodle", price: 9.25, ingredients: ["noddles", "chicken"] },
+  ],
+  reviews: [
+    { user_id: 1, rating: 5 },
+    { user_id: 2, rating: 3 },
+  ],
+}
+res2 = {
+  id: 2,
+  name: "Albertos",
+  location: {
+    city: "SLC",
+    state: "UT",
+  },
+  delivery: false,
+  days_open: "mon,tues,wed,thur,fri,sat",
+  likes: 500,
+  dishes: [
+    { name: "Tacos", price: 8.25, ingredients: ["tortilla", "carne"] },
+    { name: "Quesidalla", price: 9.25, ingredients: ["tortilla", "cheese"] },
+  ],
+  reviews: [
+    { user_id: 1, rating: 2 },
+    { user_id: 2, rating: 4 },
+  ],
+}
+
+def show_user_name(user, res)
+  if user[:id] == res
+    puts "#{user[:name]}"
+  end
+end
+
+show_user_name(user1, res2)
